ead3a67f13e711a1cf2f36f0d14cb73c48f46d2c
Arkadiusz Benedykt <arkadiusz@benedykt.net>
2018-12-05 11:47:53 +0100

    pop with factory

---
+++ PageObjectFactoryExample/NotePage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace PageObjectFactoryExample
+{
+    internal class NotePage
+    {
+        private IWebDriver _browser;
+
+        public NotePage(IWebDriver browser, Uri url)
+        {
+            _browser = browser;
+            _browser.Navigate().GoToUrl(url);
+            PageFactory.InitElements(_browser, this);
+        }
+
+        [FindsBy(How = How.CssSelector, Using = ".entry-title")]
+        public IWebElement NoteTitle { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = ".entry-content")]
+        public IWebElement NoteContent { get; set; }
+
+        public string Title => NoteTitle.Text;
+        public string Content => NoteContent.Text;
+    }
+}
